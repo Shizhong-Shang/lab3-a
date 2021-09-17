50a60181c2ebbd2a91ac957c5ffeef3b98873fb9
Shizhong <shizhongshang@cmail.carleton.ca>
2021-09-17 12:51:52 -0400

    add "address" and "phone number " to AddressBook class

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class AddressBook {
+
+    ArrayList<BuddyInfo> bList = new ArrayList<BuddyInfo>();
+
+    private void addBuddy(BuddyInfo buddy) {
+        bList.add(buddy);
+    }
+
+    private void removeBuddy(BuddyInfo buddy) {
+        bList.remove(buddy);
+    }
+
     public static void main(String[] args){
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom","Carleton","613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
